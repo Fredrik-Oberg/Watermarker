7ea031541a18fa67b5cf6c083633d0791c88e40c
Fredrik <Fredrik.oberg@outlook.com>
2014-05-27 14:57:23 +0200

    Started plugin data for creating datatype watermarker

---
+++ UmbracoDev/App_Plugins/Watermarker/watermarker.controller.js
@@ -0,0 +1,21 @@
+﻿angular.module("umbraco").controller("WatermarkerController",
+//inject umbracos assetsService
+function ($scope, assetsService, dialogService) {
+   
+
+    assetsService
+        .load([
+
+            //Ladda pluginet
+            //"/App_Plugins/Redactor/lib/.js"
+        ]).then(function () {
+
+
+
+
+
+            //assetsService.loadCss("/App_Plugins/redactor/lib/.css");
+
+
+    });
+});
