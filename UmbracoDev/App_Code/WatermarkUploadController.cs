b15e5fa8d2df46f78189b0eb780d4c1b88d54cee
Fredrik <Fredrik.oberg@outlook.com>
2014-06-04 00:07:44 +0200

    Finally added the prevalue files

---
+++ UmbracoDev/App_Code/WatermarkUploadController.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.WebApi;
+using Umbraco.Core.Models;
+
+
+public class WatermarkUploadController : UmbracoApiController
+{
+  
+    [HttpPost]
+    public HttpResponseMessage UploadWatermark()
+    {
+   
+
+        var link = "";
+        var fileName = "";
+        HttpResponseMessage result = null;
+        var httpRequest = HttpContext.Current.Request;
+        if (httpRequest.Files.Count > 0)
+        {
+            foreach (string file in httpRequest.Files)
+            {
+                var mediaService = Services.MediaService;
+                var postedFile = httpRequest.Files[file];
+                var umbracoMediaType = "Image";
+                //TODO Kolla Contenttype och sätt som rätt umbMediaType 
+                switch (postedFile.ContentType.ToLower())
+                {
+                    case "image/jpeg":
+                        break;
+                    case "application/pdf":
+                        umbracoMediaType = "File";
+                        break;
+                }
+                		
+
+                var newImage = mediaService.CreateMedia(postedFile.FileName, -1, umbracoMediaType);
+                newImage.SetValue("umbracoFile", postedFile);
+
+                mediaService.Save(newImage);
+
+                link = newImage.GetValue("umbracoFile") as string;
+                fileName = newImage.Name;
+
+            }
+            result = Request.CreateResponse(HttpStatusCode.Created, new { filelink = link, filename = fileName });
+        }
+        else
+        {
+            result = Request.CreateResponse(HttpStatusCode.InternalServerError);
+        }
+
+        return result; 
+
+    }
+
+
+}
---
+++ UmbracoDev/App_Plugins/Watermarker/watermarkerPrevalues.controller.js
@@ -0,0 +1,44 @@
+﻿angular.module("umbraco").controller("WatermarkerPrevaluesController",
+    function ($scope, $http, $timeout) {
+        //http://shazwazza.com/post/Uploading-files-and-JSON-data-in-the-same-request-with-Angular-JS
+        $scope.files = [];
+        if (!$scope.model.value) {
+            $scope.model.value = [];
+        } else {
+            $scope.file = $scope.model.value;
+        }
+        $scope.$on("filesSelected", function (event, args) {
+
+        $scope.$apply(function () {
+            $scope.files.push(args.files[0]);
+        });
+            //Show a loader 
+            $http({
+                    method: 'POST',
+                    url: "Api/WatermarkUpload/UploadWatermark",
+
+                    headers: { 'Content-Type': false },
+
+                    transformRequest: function(data) {
+                        var formData = new FormData();
+                        formData.append("model", angular.toJson(data.model));
+                        formData.append("file", data.files[0]);
+                        return formData;
+                    },
+                    data: {
+                        model: $scope.model,
+                        files: $scope.files
+                    }
+                }).
+                success(function(data, status, headers, config) {
+
+                    $scope.model.value = data.filelink;
+                    $scope.file = $scope.model.value;
+                    $scope.hasError = false;
+                    return;
+                }).error(function(data, status, headers, config) {
+                    $scope.hasError = true;
+
+                });
+    }); 
+});
